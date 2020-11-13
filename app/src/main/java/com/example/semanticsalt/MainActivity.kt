dde6ad6a66efcd62393cbb4069336b31d17350fd
mjenkins-dev <54869556+mjenkins-dev@users.noreply.github.com>
2020-11-12 23:10:31 -0500

    first working kotlin program

--- app/src/main/java/com/example/semanticsalt/MainActivity.kt
+++ app/src/main/java/com/example/semanticsalt/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.semanticsalt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var id = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        count.text = id.toString()
+
+        plusBtn.setOnClickListener {
+            ++id
+            count.text = id.toString()
+        }
+
+        minusBtn.setOnClickListener {
+            --id
+            count.text = id.toString()
+        }
     }
+
 }
