607e97c7a677100d7faacb91a60466e628a72712
mjenkins-dev <54869556+mjenkins-dev@users.noreply.github.com>
2020-11-12 23:23:43 -0500

    first working kotlin program now no errors!

--- app/src/main/java/com/example/semanticsalt/MainActivity.kt
+++ app/src/main/java/com/example/semanticsalt/MainActivity.kt
@@ -6,12 +6,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var id = 0
+    private var id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         count.text = id.toString()
 
